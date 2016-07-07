127da9bd69673252c408b26e359763d32516ed4b
graysomb <micharlg2767@gmail.com>
2016-07-07 11:33:06 -0400

    created project

---
+++ QVCscrape/QVCscrape/spiders/QVC.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from QVCscrape.items import QVCItem
+from scrapy.linkextractors import LinkExtractor
+from igraph import *
+
+
+class QVCSpider(scrapy.Spider):
+	name = "QVC"
+	allowed_domains = ["qvc.com"]
+	start_urls = ["http://www.qvc.com/webapp/wcs/stores/servlet/ProgramGuideDailyView?TimeZoneSelect=EST&cm_sp=24Hr-_-HEAD-_-Desc&cm_re=MH-_-SHOPQVCLIVE-_-PROGRAMGUIDE&storeId=10251&catalogId=10151&langId=-1&Navigate=20160707&channelCode=QVC"]
+
+
+	def parse(self, response):
+
+		#iterate over links
+		#for sel in response.xpath('//@href'):
+		links = LinkExtractor(allow=self.allowed_domains, deny=()).extract_links(response)
+		for sel in links:
+			request = scrapy.Request(node['link'], self.follow_the_trail)
+			yield request
+
+
+		# for sel in response.xpath('//@href'):
+		# 	item = DmozItem()
+		# 	item['title'] = sel.xpath('a/text()').extract()
+		# 	item['link']=link = sel.xpath('a/@href').extract()
+		# 	item['desc'] = sel.xpath('text()').extract()
+		# 	print item['link']
+
+	#follow links recursively until no unique links exist
+	def follow_the_trail(self,response):
+		links = LinkExtractor(allow=self.allowed_domains, deny=()).extract_links(response)
+		for sel in links:
+			request = scrapy.Request(node['link'], self.follow_the_trail)
+			yield request
+
+	def closed(self, reason):
+		pass
+		
+
+    	
